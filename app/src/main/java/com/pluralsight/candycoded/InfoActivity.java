ba345740204aa32b5e7de16d43c4bb4e7ec8da8b
Lonwabo Msingelwa <lmsingelwa@gmail.com>
2019-10-25 12:17:31 +0200

    Completed Module 2

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,18 +1,26 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
 public class InfoActivity extends AppCompatActivity {
 
+    View tvAddress;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        tvAddress = findViewById(R.id.text_view_address);
 
         Uri uri = Uri.parse("android.resource://com.codeschool.candycoded/" + R.drawable.store_front);
         ImageView candyStoreImageView = (ImageView)findViewById(R.id.image_view_candy_store);
@@ -20,7 +28,23 @@ public class InfoActivity extends AppCompatActivity {
                 load(uri).
                 into(candyStoreImageView);
 
+        createMapIntent(tvAddress);
+    }
 
+    public void createMapIntent(View view)
+    {
+        Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        if (!mapIntent.resolveActivity(getPackageManager()).equals(null))
+        {
+            startActivity(mapIntent);
+        }
+        else
+        {
+            Toast.makeText(this, "Null", Toast.LENGTH_SHORT).show();
+        }
     }
 
     // ***
